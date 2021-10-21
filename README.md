6d35c2fd14372be3c78329df849c4e29c670ee95
Wesley Lin <wesleylin625@gmail.com>
2021-10-20 21:07:04 -0700

    add seestone list books

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 
 
   seeing stones
+
+  cli for the one api using urfave/cli
+
+
+
+https://the-one-api.dev/
+
+
+
+
+
+```
+  go run main.go
+```
+
+```
+  go install
+  $GOPATH/bin/seestone
+
+  # or for windows
+  $GOPATH\\bin\\seestone
+```
+
--- main.go
+++ main.go
@@ -1,12 +1,77 @@
 package main
 
-
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
+type book struct {
+	ID   string `json:"_id"`
+	Name string `json:"name"`
+}
+
+type listBookRespBody struct {
+	Docs []book `json:"docs"`
+}
+
 func main() {
-	(&cli.App{}).Run(os.Args)
+	var app *cli.App = &cli.App{
+		Name:  "books",
+		Usage: "list books",
+		Action: func(c *cli.Context) error {
+			fmt.Println("main")
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "books",
+				Action: func(c *cli.Context) error {
+					listBooks()
+					return nil
+				},
+			},
+		},
+	}
+	// (&cli.App{}).Run(os.Args)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func listBooks() {
+	var url = "https://the-one-api.dev/v2/book"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	// var data map[string]interface{}
+
+	var respBody listBookRespBody
+
+	// if err := json.Unmarshal(resp.Body, &data); err != nill {
+	err = json.NewDecoder(resp.Body).Decode(&respBody)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(respBody.Docs)
+}
+
+func respToString(resp http.Response) string {
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(bytes)
 }
